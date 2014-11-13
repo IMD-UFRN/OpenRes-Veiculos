8dc96e3eceba0c5dbf4e3babe7468414c9c17ec2
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-11-13 08:45:59 -0300

    Add mock admins for reservation preview

--- app/assets/javascripts/angular/controllers/vehicle_reservation_controller.js
+++ app/assets/javascripts/angular/controllers/vehicle_reservation_controller.js
@@ -1,14 +1,17 @@
-OpenResVeiculosApp.controller('CarReservationsCtrl', function ($scope, $http) {
+OpenResVeiculosApp.controller('CarReservationsCtrl', function ($scope, $http, $location) {
   $scope.preview = {};
 
   $scope.date_selected = moment().format("D/M/YYYY");
+
+  $("#vehicle_reservation_vehicle_id").attr("chosen", ($location.search()).vehicle_id);
+  $("#vehicle_reservation_vehicle_id").trigger('chosen:updated');
 
   $scope.updatePreview = function(){
 
     if($scope.vehicle == undefined || $scope.vehicle == 0){
       return;
     }
-    
+
     $http.get('/vehicles/'+$scope.vehicle+'/reservations.json?date=' + $scope.date_selected)
       .success(function(data) {
         $scope.preview = data;
